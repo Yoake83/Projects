18169fef273e5a1246cd7e10c73fc1befa0bfd7c
Yoake <devi230101083@iiitmanipur.ac.in>
2024-11-23 11:27:35 +0530

    Project 3

---
+++ demo.js
@@ -0,0 +1,9 @@
+// let abc=1,bcd=2.2,cde="4";
+// console.log(abc+bcd==2.2,bcd+0.2===2.4,cde+abc,cde-abc===3);
+// console.log(abc+bcd);
+// console.log(b+true);
+// let a=5;
+// var b;
+// console.log(a+b);
+let abc=true,bcd=5,def=null;
+console.log(abc+def,abc**-bcd);
